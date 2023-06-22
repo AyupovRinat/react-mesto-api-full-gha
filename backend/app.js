20f882a87e29ed6b24128039b4f9f7b2e179cfe9
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-06-22 23:41:21 +0500

    5 коммит

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
@@ -21,12 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 app.post('/signin', signinValidator, login);
 app.post('/signup', signupValidator, createUser);
